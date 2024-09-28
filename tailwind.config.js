083d227aa64bf0947a69b91f72d0770908d323ca
Zeinab <zeinabansarian6280@gmail.com>
2024-09-28 20:10:58 +0330

    complete contact us page

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,7 @@ module.exports = {
       'IRANSansWeb(FaNum)700': ['"IRANSansWeb(FaNum)700"'],
       'IRANSansWeb(FaNum)400': ['"IRANSansWeb(FaNum)400"'],
       'sgKara400': ['"sgKara400"'],
+      'Inter400': ['"Inter400"'],
       'IRANYekan300': ['"IRANYekan300"'],
       'IRANYekan400': ['"IRANYekan400"'],
       'IRANYekan500': ['"IRANYekan500"'],
