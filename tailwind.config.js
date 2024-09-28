05ba6e641c031fa6520ad53fcc3f72f019eb2681
Zeinab <zeinabansarian6280@gmail.com>
2024-09-28 19:07:58 +0330

    complete project list page and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,7 @@ module.exports = {
       'IranYekan700': ['IranYekan700', 'IranYekan700'],
       'IRANSansWeb(FaNum)700': ['"IRANSansWeb(FaNum)700"'],
       'IRANSansWeb(FaNum)400': ['"IRANSansWeb(FaNum)400"'],
+      'sgKara400': ['"sgKara400"'],
       'IRANYekan300': ['"IRANYekan300"'],
       'IRANYekan400': ['"IRANYekan400"'],
       'IRANYekan500': ['"IRANYekan500"'],
