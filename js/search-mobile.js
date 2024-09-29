4df7dbc23691adf6ca0edf816b1c140461d4943e
Zeinab <zeinabansarian6280@gmail.com>
2024-09-29 11:25:49 +0330

    complete search page

---
+++ js/search-mobile.js
@@ -0,0 +1,22 @@
+const articleSlider = new Swiper('.articleSlider', {
+    // Optional parameters
+   speed : 1000,
+   slidesPerView :1.2,
+   spaceBetween : 30,
+  
+  });
+const proSlider = new Swiper('.proSlider', {
+    // Optional parameters
+   speed : 1000,
+   slidesPerView :1.2,
+   spaceBetween:28,
+  
+  });
+const projectSlider = new Swiper('.projectSlider', {
+    // Optional parameters
+   speed : 1000,
+   slidesPerView : 1.1,
+   spaceBetween : 32,
+   
+  
+  });
