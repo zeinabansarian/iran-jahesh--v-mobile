af50a31a2a5fa55cb2ac22b451e53f51d58d28bf
faezehdev <faezeh0078@gmail.com>
2024-09-29 15:56:45 +0330

    complete blog pge

---
+++ js/articlelistMobile.js
@@ -0,0 +1,42 @@
+$(function(){
+    var current = window.location.search;
+   if( current.includes("pageno")){
+    $('.Paginations a').each(function(){
+        var $this = $(this);
+        let  str = current.split("pageno=")[1];
+        let result = str.substr(0,1);
+        if($this.attr('id') == result){
+            $this.addClass('active');
+            $this.siblings().removeClass( 'active' );
+        }      
+    })
+  }
+  else{
+    $('#1').addClass('active');
+  }
+  })
+   // Enable Scroll
+  
+    const lenis = new Lenis()
+  
+  
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+    gsap.registerPlugin(ScrollTrigger);
+  let blogs = document.querySelectorAll('.Blog')
+  console.log(blogs);
+  
+  blogs.forEach(blog=>{
+      gsap.to(blog.querySelector('.Title h2'),{
+          y:0,
+          scrollTrigger: {
+              trigger:blog,
+            start: "top 70%", 
+            end:'bottom bottom',
+          },
+      })
+  })
